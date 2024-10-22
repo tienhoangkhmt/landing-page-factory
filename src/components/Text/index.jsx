41d023955be3178b9ff8dfbd3f1bd12cca5ccbf4
hoangnt <nguyentienhoang@MacBook-Air-cua-Nguyen.local>
2024-10-22 22:54:33 +0700

    fix: class name component text

--- src/components/Text/index.jsx
+++ src/components/Text/index.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-
 function Text(props) {
   return (
-    <p className={`sm:text-sm text-[12px] ${props.class}`}>{props?.children}</p>
+    <p className={`sm:text-sm text-[12px] ${props.class || props?.className}`}>
+      {props?.children}
+    </p>
   );
 }
 
